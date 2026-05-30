7c46c01dd34861187922702f33b1d1185d3478d8
rushindra1404 <rushindrabandaru1404@gmail.com>
2026-05-30 16:13:17 +0530

    Replace GSAP trial package with official free GSAP library

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { AnimatePresence, motion } from "framer-motion";
 import "./styles/Navbar.css";
 
